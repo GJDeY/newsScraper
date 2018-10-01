0f94fd8dde72a719f6e277552f6e49da10def1e0
GJDeY <gregoryjdeyoung@u.northwestern.edu>
2018-10-01 10:04:51 -0500

    changes

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ var db = require("./models");
 //set port to 8080 or an available port! if not working before deployment, try 3000
 var PORT = process.env.PORT || 8080;
 var app = express();
-var url = "https://www.bleedcubbieblue.com/chicago-cubs-news"
 
 // request(url, function(err, res, body){
 // var $ = cheerio.load(body);
@@ -26,28 +25,22 @@ mongoose.connect("mongodb://localhost/newsScraper", { useNewUrlParser: true });
 
 
 app.get("/scrape", function (req, res) {
-    axios.get("http://www.echojs.com/").then(function (response) {
+    axios.get("https://www.bleedcubbieblue.com/chicago-cubs-news").then(function (response) {
 
         var $ = cheerio.load(response.data);
 
         //grab h2 tags and put into an object
         $("h2 class").each(function (i, element) {
             var result = {};
+        )
 
-            //aticle element tags which i want to scrape from below 
-            //<h2 class="c-entry-box--compact__title"><a data-chorus-optimize-field="hed" data-analytics-link="article" href="https://www.bleedcubbieblue.com/2018/9/29/17914178/cubs-social-media-night">A look at Cubs social media night </a></h2>
+        //aticle element tags which i want to scrape from below 
+        //<h2 class="c-entry-box--compact__title"><a data-chorus-optimize-field="hed" data-analytics-link="article" href="https://www.bleedcubbieblue.com/2018/9/29/17914178/cubs-social-media-night">A look at Cubs social media night </a></h2>
 
-
-            //parse the body objects
-
-
+        //parse the body objects
 
 
 
 
-            app.listen(PORT);
-            console.log('server running on PORT ' + PORT);
-
-
-
-
+        app.listen(PORT);
+        console.log('server running on PORT ' + PORT);
